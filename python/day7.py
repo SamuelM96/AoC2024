1f528413c9a18bd0f5ba1dd822fe52ce4fedef7d
Samuel Mott <samuel.e.mott@gmail.com>
2024-12-07 16:24:59 +0000

    perf(day7): upgraded to a recusive algorithm

--- python/day7.py
+++ python/day7.py
@@ -1,44 +1,46 @@
 import time
 
-def solve_part1(input):
+
+def part1(nums, goal, pos, value):
+    if pos == len(nums) - 1:
+        return value == goal
+    num = nums[pos + 1]
+    return part1(nums, goal, pos + 1, value + num) or part1(
+        nums, goal, pos + 1, value * num
+    )
+
+
+def part2(nums, goal, pos, value):
+    if pos == len(nums) - 1:
+        return value == goal
+    num = nums[pos + 1]
+    return (
+        part2(nums, goal, pos + 1, value + num)
+        or part2(nums, goal, pos + 1, value * num)
+        or part2(nums, goal, pos + 1, value * (10 ** len(str(num))) + num)
+    )
+
+
+def solve(input, check):
     count = 0
     for line in input.splitlines():
         numbers = line.split()
         goal = int(numbers[0][:-1])
         nums = [int(n) for n in numbers[1:]]
 
-        results = [nums[0]]
-        for num in nums[1:]:
-            results = (
-                [a + num for a in results]
-                + [a * num for a in results]
-            )
-        count += goal if goal in results else 0
-
-    return count
-
-def solve_part2(input):
-    count = 0
-    for line in input.splitlines():
-        numbers = line.split()
-        goal = int(numbers[0][:-1])
-        nums = [int(n) for n in numbers[1:]]
-
-        # A set performs worse for this use case due to the additional
-        # overhead versus input size, so the list is best
-        results = [nums[0]]
-        for num in nums[1:]:
-            # Using the op method reduces the number of list creations
-            # and additions. 2.4s -> 1.8s on my system
-            results = [
-                val * num if op == 0 else
-                val + num if op == 1 else
-                val * (10 ** len(str(num))) + num
-                for val in results
-                for op in range(3)
-            ]
-        count += goal if goal in results else 0
-
+        # A recursive algorithm appears to be the most performant
+        # compared to using other data structures (stacks, dicts, sets, etc).
+        #
+        # Likely because the stack allocation for function frames are trivial
+        # and common enough to be optimised, whereas data structures have
+        # additional overhead (hashing + allocations). Pre-allocation helps a bit,
+        # but still lost the recursive option.
+        #
+        # Only potential downside is running out of stack space if recursion gets
+        # too deep, but it's fine in this case.
+        #
+        # Also has the benefit of being able to swap out implementations easily
+        count += goal if check(nums, goal, 0, nums[0]) else 0
     return count
 
 
@@ -46,8 +48,8 @@ if __name__ == "__main__":
     with open("day7.input", "r") as f:
         input = f.read()
 
-    print("Part 1:", solve_part1(input))
+    print("Part 1:", solve(input, part1))
     start = time.perf_counter()
-    result = solve_part2(input)
+    result = solve(input, part2)
     end = time.perf_counter()
     print(f"Part 2: {result} => {end-start:.6f} seconds")
